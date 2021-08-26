646350b0f535a232bf6bec493cf5fef222baf1e9
Sue Xiong <sxiong0519@gmail.com>
2021-08-26 14:57:57 -0500

    phase 1 completed

--- guessinggame/Program.cs
+++ guessinggame/Program.cs
@@ -6,7 +6,12 @@ namespace guessinggame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Guess the Secret Number! Do you want to play?");
+            Console.WriteLine("Pick a number between 1 and 100...");
+
+            string num = Console.ReadLine();
+
+            Console.WriteLine($"You answered {num}.... Let's see if you're correct.");
         }
     }
 }
