07daefb59f9a076da15c34f031b4cb87a95031fd
Sue Xiong <sxiong0519@gmail.com>
2021-08-27 11:34:21 -0500

    added guess limit

--- guessinggame/Program.cs
+++ guessinggame/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// The program should be updated to...
+
+// Give the user four chances to guess the number.
+// Continue to display the success or failure messages as in phase 2
+
+using System;
 
 //name of the "space" where data is being created... "dotnet new console -o projname"
 //will most likely use this during times when needing to pull class/methods
@@ -7,27 +12,29 @@ namespace guessinggame
     //normally the same as the .cs file.
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {   
+            int secretNum = 42;
             //logging line 13 and 14 
-            Console.WriteLine("Welcome to Guess the Secret Number! Do you want to play?");
-            Console.WriteLine("Pick a number between 1 and 100...");
-            //string is the class... num is the variable... console.readline is code for waiting for response of the user.. once user provides a response it will then be logged into "num" the variable
-            string num = Console.ReadLine();
-            //pulling the response number for use in {}...
-            Console.WriteLine($"You answered {num}.... Let's see if you're correct.");
-            //string secret instead of "int" because you can't compare a string to an integer in the if statement...
-            string secret = "42";
+            
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Welcome to Guess the Secret Number! Do you want to play?");
+                Console.WriteLine("Pick a number between 1 and 100...");
+                //variable "num" is identified as an integer class... therefore must parse int console.readline since readline always comes back as a string
+                int num = int.Parse(Console.ReadLine()); 
             //comparing response to the secret number.... If the same answer then yay... if not the same.. wrong... 
-            if (num == secret) 
-            {
-                Console.WriteLine("Y A Y !!! You got it correct!");
-            } 
-            else 
-            {
-                Console.WriteLine("Wrong. Do better.");
-            }
+                if (num == secretNum) 
+                {
+                    Console.WriteLine("Y A Y !!! You got it correct!");
+                } 
+                else 
+                {
+                    Console.WriteLine("Wrong. Do better.");
+                }
+            }        
         }
+
     }
 }
 
